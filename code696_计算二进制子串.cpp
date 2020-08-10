00da1d46d96a26805f6856b6c861de2e295b38f3
luanjianbing <794315704@qq.com>
2020-08-10 19:18:13 +0800

    update

---
+++ code696_计算二进制子串.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+	int countBinarySubstrings(string s) {
+		vector<int> counts;
+		int ptr = 0, n = s.size();
+		while (ptr < n) {
+			char c = s[ptr];
+			int count = 0;
+			while (ptr < n && s[ptr] == c) {
+				++ptr;
+				++count;
+			}
+			counts.push_back(count);
+		}
+		int ans = 0;
+		for (int i = 1; i < counts.size(); ++i) {
+			ans += min(counts[i], counts[i - 1]);
+		}
+		return ans;
+	}
+};
+
+int main() {
+	string str = "00110011";
+	Solution s;
+	int res = s.countBinarySubstrings(str);
+	cout << res << endl;
+
+	return 0;
+}
