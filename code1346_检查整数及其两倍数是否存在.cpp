6f94510a527d9b3f17bd23513216785f0fe8e3a8
luanjianbing <794315704@qq.com>
2020-08-14 11:14:10 +0800

    update

---
+++ code1346_检查整数及其两倍数是否存在.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+//给你一个整数数组 arr，请你检查是否存在两个整数 N 和 M，
+//满足 N 是 M 的两倍（即，N = 2 * M）。
+//
+//更正式地，检查是否存在两个下标 i 和 j 满足：
+//
+//i != j
+//0 <= i, j < arr.length
+//arr[i] == 2 * arr[j]
+
+
+class Solution {
+public:
+	bool checkIfExist(vector<int>& arr) {
+		if (arr.empty()) return false;
+		unordered_map<int, int> pairs;
+		int countZero = 0;
+		for (int i = 0; i < arr.size(); i++) {
+			if (arr[i] == 0)countZero++;
+			else pairs.insert(pair<int, int>(arr[i], arr[i] * 2));
+		}
+		for (int i = 0; i < arr.size(); i++) {
+			if (arr[i] == 0) {
+				if (countZero > 1)
+					return true;
+			}
+			else {
+				if (pairs.count(pairs[arr[i]]))
+					return true;
+			}
+		}
+		return false;
+	}
+};
+
+int main() {
+	vector<int> arr = { 7, 1, 14, 11 };
+	Solution s;
+	cout << s.checkIfExist(arr) << endl;
+
+	return 0;
+}
---
+++ code20_有效的括号.cpp
@@ -0,0 +1,82 @@
+﻿#include <iostream>
+#include <unordered_map>
+#include <stack>
+
+using namespace std;
+
+//给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
+//
+//有效字符串需满足：
+//
+//左括号必须用相同类型的右括号闭合。
+//左括号必须以正确的顺序闭合。
+//注意空字符串可被认为是有效字符串。
+
+
+class Solution {
+public:
+	bool isValid(string s) {
+		//// 返回true的情况只有两种(错误)
+		//// 1. 顺序 2.中心对称
+		//int lenStr = s.length();
+		//if (!lenStr) return true;
+		//bool isSituationOne = true;
+		//if (lenStr % 2) return false;
+		//for (int i = 0; i < lenStr; i += 2) {
+		//	if (!(s[i + 1] - s[i] > 0 && s[i + 1] - s[i] < 5))
+		//		isSituationOne = false;
+		//}
+		//if (isSituationOne)
+		//{
+		//	return true;
+		//}
+		//else
+		//{
+		//	int mid = lenStr / 2;
+		//	int ptrLeft = mid - 1;
+		//	int ptrRight = mid;
+		//	while (ptrLeft >= 0) {
+		//		if (!(s[ptrRight] - s[ptrLeft] > 0 && s[ptrRight] - s[ptrLeft] < 5))
+		//			break;
+		//		ptrLeft--;
+		//		ptrRight++;
+		//	}
+
+		//	if (ptrLeft < 0)
+		//		return true;
+		//	return false;
+		//}
+
+		// 使用栈结构
+		int n = s.length();
+		if (n % 2)return false;
+
+		unordered_map <char, char> pairs = {
+			{')', '('}, {']', '['}, {'}', '{'}
+		};
+
+		stack<char> myStack;
+		for (char ch : s) {
+			if (pairs.count(ch)) {
+				if (myStack.empty() || myStack.top() != pairs[ch]) {
+					return false;
+				}
+				myStack.pop();
+			}
+			else {
+				myStack.push(ch);
+			}
+		}
+
+		return myStack.empty();
+	}
+};
+
+int main() {
+	string str = "([)]";
+	Solution s;
+	bool res = s.isValid(str);
+	cout << res << endl;
+
+	return 0;
+}
